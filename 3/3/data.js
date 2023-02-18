1e740ea162ca74ce79bfda1ca32b2c7237ef0388
deli-off <dzamsedovnazif1@gmail.com>
2023-02-18 22:43:16 +0500

    add

---
+++ 3/3/data.js
@@ -0,0 +1,106 @@
+const data = [
+    {
+        title: 'MacBook Air',
+        img: 'macbookair.png',
+        price: 999,
+        url: 'https://www.apple.com/shop/buy-mac/macbook-air',
+        colors: ['#C7C8CA', '#B1B2B7', '#E3CCB4'],
+        specs: {
+            display: {
+                title: 'Retina display',
+                size: 13.3
+            },
+            chip: {
+                title: 'Apple M1 chip',
+                description: ''
+            },
+            ram: {
+                title: 'Up to 16GB unified memory',
+                description: 'For increased performance and power efficiency'
+            },
+            memory: {
+                size: 2,
+                type: 'tb'
+            },
+            battery: 18,
+            camera: {
+                title: '720p FaceTime HD camera',
+                description: 'With the image signal processor of M1 for drastically improved performance'
+            },
+            weight: 2.8,
+            other: [
+                'Touch ID'
+            ]
+        }
+    },
+    {
+        title: 'MacBook Pro 13”',
+        price: 1299,
+        url: 'https://www.apple.com/shop/buy-mac/macbook-pro/13-inch',
+        colors: ['#C7C8CA', '#B1B2B7'],
+        specs: {
+            display: {
+                title: 'Retina display',
+                size: 13.3
+            },
+            chip: {
+                title: 'Apple M1 chip',
+                description: 'Also available with Intel Core i5 or i7 processor'
+            },
+            ram: {
+                title: 'Up to 16GB unified memory',
+                description: 'For increased performance and power efficiency'
+            },
+            memory: {
+                size: 2,
+                type: 'tb'
+            },
+            battery: 20,
+            camera: {
+                title: '720p FaceTime HD camera',
+                description: 'With the image signal processor of M1 for drastically improved performance'
+            },
+            weight: 3.0,
+            other: [
+                'Touch Bar',
+                'Sound',
+                'Touch ID'
+            ]
+        }
+    },
+    {
+        title: 'MacBook Air',
+        price: 999,
+        url: 'https://www.apple.com/shop/buy-mac/macbook-pro/16-inch',
+        colors: ['#C7C8CA', '#B1B2B7', '#E3CCB4'],
+        specs: {
+            display: {
+                title: 'Retina display',
+                size: 16
+            },
+            chip: {
+                title: 'Up to Intel Core i9 processor',
+                description: ''
+            },
+            ram: {
+                title: 'Up to 64GB memory',
+                description: ''
+            },
+            memory: {
+                size: 8,
+                type: 'tb'
+            },
+            battery: 11,
+            camera: {
+                title: '720p FaceTime HD camera',
+                description: ''
+            },
+            weight: 4.3,
+            other: [
+                'Touch Bar',
+                'Sound',
+                'Touch ID'
+            ]
+        }
+    },
+]
